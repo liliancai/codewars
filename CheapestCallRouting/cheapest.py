12a82fc42256d664d4100328a20ff12f41c83505
liliancai <yushunchn@gmail.com>
2019-02-21 23:08:07 +0100

    basic done, need refactor

--- CheapestCallRouting/cheapest.py
+++ CheapestCallRouting/cheapest.py
@@ -1,44 +1,72 @@
+import operator
 #from the longest prefix search 
 #reorder each list according to price
 #input as a .text file
 
-def routing(number):
+def Routing(number):
 	if number=="":
 		return False
-	ConvertInputText()
+	OperatorNames,Lists=ConvertInputText()
+
+	#resort each list by lowest price
+	Sorted_Lists=list()
+	for each in Lists:
+		sorted_e=sorted(each.items(),key=operator.itemgetter(1))
+		Sorted_Lists.append(sorted_e)
+
+	#check number from longest prefix
+	preFixes=list()
+	for i in range(1,len(number)+1):
+		preFix=number[:i]
+		preFixes.insert(0,preFix)
+	
+	OperotorName=""	
+	lowestPrice=10000 #set price bit of impossible high
+	for preFix in preFixes:
+		for i,eachList in enumerate(Lists):
+			if preFix in eachList:
+				if float(eachList[preFix])<lowestPrice:
+					#save the lowest price
+					lowestPrice=eachList[preFix]
+					OperotorName=OperatorNames[i]
+			if lowestPrice !=10000:
+				return lowestPrice,OperotorName 
 
 
+
+#breack data into a set of dictionary
 def ConvertInputText():
 	try:
-		#change test to txt
-		with open("input.test") as f:
+		with open("input.txt") as f:
 			data=f.read()
 	except FileNotFoundError:
 		raise FileNotFoundError("You need to put operators list in input.txt")
 
+	#for easier to sperate each Operator's name and list
 	data=data.replace("\n\n",";")
 	data=data.replace(":\n",";")
-	data=data[:-1]	
+	#remove the last extra \n,otheriwise split the list into dict won't work 
+	data=data[:-1]
 	
-	lists=data.split(";")
-#	print(lists)
+	Lists=data.split(";")
+	OperatorNames=list()
 
-	OperatorNames=set()
-	PriceList=list()
-
-	for i in range(len(lists)):
+	for i in range(len(Lists)):
 		if i%2==0:
-			OperatorNames.add(lists[i])
-		else:	
-			item=dict((x.strip(),y.strip()) for x,y in (e.split('\t ') for e in lists[1].split('\n')))
-			PriceList.append(item)
-	print(PriceList)
-	print(OperatorNames)
-	#breack data into a set of dictionary
-	#reorder each section
-	#check number from longest prefix
-	#save the lowest price
-	#return Operatin name and price
+			OperatorNames.append(Lists[i])
+		else:
+			Lists[i]=dict((x.strip(),y.strip()) for x,y in (e.split('\t ') for e in Lists[i].split('\n')))
+	for each in Lists:
+		if each[0]=="O":
+			Lists.remove(each)
+
+	return OperatorNames,Lists
 
 if __name__=="__main__":
-	routing(1)
+	number=input("Please dial your number:\n")
+	result=Routing(number)
+	if result!=None:
+		print("The lowest price is ",result)
+	else:
+		print("The number you dailed is unreachable.")
+		
--- CheapestCallRouting/test.py
+++ CheapestCallRouting/test.py
@@ -1,21 +1,29 @@
 import os,unittest
+from unittest import skip
 import cheapest
+
 class CheapestTest(unittest.TestCase):
 	with open('input.test','w+') as f:
 		f.write("test")
 
 	def test_empty_string_error(self):
-		print("test")
-		self.assertEqual(cheapest.routing(""),False)
+		self.assertEqual(cheapest.Routing(""),False)
 	
+	@skip
 	def test_if_txt_file_does_not_exist(self):
 		try:
 			#remove input.test if exist
 			os.remove("input.test")
 		except FileNotFoundError:
 			pass
-		#check if 404error raised in routing
-		self.assertRaises(FileNotFoundError,cheapest.routing,123)
+		#check if 404error raised in Routing
+		self.assertRaises(FileNotFoundError,cheapest.Routing,'123')
 
+	def test_routing_result(self):
+		self.assertEqual(cheapest.Routing("1"),('0.9','Operator A'))
+		self.assertEqual(cheapest.Routing("46732"),('1.1','Operator A'))
+		self.assertEqual(cheapest.Routing("44"),('0.5','Operator B'))
+		self.assertEqual(cheapest.Routing("22"),None)
+			
 if __name__=="__main__":
 	unittest.main()
