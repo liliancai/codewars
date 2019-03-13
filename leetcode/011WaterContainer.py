6f2835bb02ba13c4fbd8498b1524cb645f2577a7
CaiYe <yushunchn@gmail.com>
2019-03-13 19:31:50 +0100

    Update 011WaterContainer.py

--- leetcode/011WaterContainer.py
+++ leetcode/011WaterContainer.py
@@ -1,6 +1,7 @@
 # Two points method
 # Cause it depends on height*width so the farest two lines are best pair
 # Hiw to have higher height to get more volume for inner lines
+#73ms 42%
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         i, j=0,len(height)-1
@@ -13,3 +14,21 @@ class Solution:
                j-=1
     
         return water#
+'''
+# 100ms 17%
+# Trying to improve runtime but got worse--!
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j=0,len(height)-1
+        water=0
+        while (i < j):
+            water=max(water,(j-i)*min(height[i], height[j]))
+            if height[i] < height[j]:
+                if height[i]<height[i+1]:i+=1
+                else:i+=2
+            else:
+                if height[j-1]>height[j]:j-=1
+                else:j-=2
+    
+        return water
+ '''
