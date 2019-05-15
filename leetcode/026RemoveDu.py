8c7865039a5e3d7b31038ef0d2d7fa3acdb6102b
Ye Cai <yushunchn@gmail.com>
2019-05-15 15:36:42 +0200

    Update 026RemoveDu.py

--- leetcode/026RemoveDu.py
+++ leetcode/026RemoveDu.py
@@ -4,3 +4,16 @@ class Solution:
         for i in range(len(new)):
             nums[i] = new[i]
         return len(new)
+-------Java-----------------
+// Runtime: 1 ms, faster than 99.88% of Java
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int n:nums){
+            if( i==0 || n > nums[i-1]){
+                nums[i++] = n;
+            }
+        }
+        return i;
+    }
+}
