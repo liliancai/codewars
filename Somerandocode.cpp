27ed74499961b4a258c129dfbcda8e3dfbdf5a03
Ye Cai <yushunchn@gmail.com>
2019-05-16 10:38:35 +0200

    Update Somerandocode.cpp

--- Somerandocode.cpp
+++ Somerandocode.cpp
@@ -1,10 +1,15 @@
-	map<string, double*> 
-  
-  myMap = map<string, double*>();
+// Example program
+#include <iostream>
+#include <cstring>
+#include <map>
+using namespace std;
+int main()
+{
+ 	map<string, double*> myMap = map<string, double*>();
 	double* aaaaa = new double[4];
 	memset(aaaaa, 0, 4 * sizeof(double));
 	for (int i = 0; i < 4; i++) 
-  aaaaa[i] = i;
+          aaaaa[i] = i;
 	myMap.insert(pair<string, double*>("aaa", aaaaa));
 	//aaaaa[2] = 0;
 	delete[] aaaaa;
@@ -15,3 +20,5 @@
 		for (int i = 0; i < 4; i++) cout << aaa[i] << ", ";
 		cout << endl;
 	}
+}
+
