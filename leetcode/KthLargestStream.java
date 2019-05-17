139ec132253acd2c1e6775752eb701e559c6cfa9
Ye Cai <yushunchn@gmail.com>
2019-05-17 23:46:54 +0200

    Create KthLargestStream.java

---
+++ leetcode/KthLargestStream.java
@@ -0,0 +1,20 @@
+//Runtime: 57 ms, faster than 99.40%
+class KthLargest {
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private int size;
+    
+    public KthLargest(int k, int[] nums) {
+        this.size = k;
+        for (int n : nums) add (n);
+    }
+    
+    public int add(int val) {
+        if (minHeap.size() < size)
+            minHeap.offer(val);
+        else if (minHeap.peek() < val) {
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+        return minHeap.peek();
+    }
+}
