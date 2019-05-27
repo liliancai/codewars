7aa665fce481e2f9748ac8712687e33b77cba44c
Ye Cai <yushunchn@gmail.com>
2019-05-27 15:03:08 +0200

    Create 415.addstrings.java

---
+++ leetcode/415.addstrings.java
@@ -0,0 +1,36 @@
+// A failed version, the input number length max 5100
+// but BigInteger's max is 2^32 4294967296
+// the result will overflow any string longer than length 10 lead 4xxx- -!
+// what a pitfall!
+impor4t java.math.BigInteger;
+class Solution{
+    
+
+    public String addStrings(String num1, String num2) {
+        int digit = 0;
+        BigInteger sum = new BigInteger("0");
+        int p1 = num1.length()-1, p2 = num2.length()-1;
+        // if(p1==-1&&p2==-1)
+        int x = 0,y=0, n=0, newLead=0;
+        
+        while(p1>=0|p2>=0|digit>0){
+            if(p1>=0) x=Character.getNumericValue(num1.charAt(p1));
+            else x=0;
+            if(p2>=0) y=Character.getNumericValue(num2.charAt(p2));
+            else y=0;
+         System.out.println(x+y);
+            // newLead = ;
+            sum = sum.add(BigInteger.valueOf(((x+y+digit)%10)*(int)Math.pow(10,n)));
+            // System.out.println(sum);
+            digit = (x+y+digit)/10;
+            // System.out.println(digit);
+            n++;
+            p1--;
+            p2--;
+        }
+        // System.out.println(sum);
+        return String.valueOf(sum);
+    }
+}
+
+-------String + string = Another string!------------------------
