e6161fd0fa107c52505c4ab2292f9c4d22cf797a
Ye Cai <yushunchn@gmail.com>
2019-05-30 11:52:29 +0200

    Update 002AddTwoNumbes.py

--- leetcode/002AddTwoNumbes.py
+++ leetcode/002AddTwoNumbes.py
@@ -27,3 +27,32 @@ class Solution(object):
         return root.next
        
     
+------------------------java 2ms--------------------
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode p3 = head, p1=l1,p2=l2;
+        int digit=0,v1,v2;
+        while(p1!=null||p2!=null||digit!=0){
+            if(p1!=null) v1=p1.val; else v1=0;
+            if(p2!=null) v2=p2.val; else v2=0;
+            
+            int sum = v1+v2+digit;
+            p3.next =new ListNode(sum%10);
+            p3=p3.next;
+            digit=sum/10;
+            if(p1!=null)p1=p1.next;
+            if(p2!=null)p2=p2.next;
+           
+        }
+        return head.next;
+    }
+}
